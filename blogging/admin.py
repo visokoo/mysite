3e0f5e19a816f14e453a437aed7c156e1176ab1c
Vivian Ta <vivian.ta@daimler.com>
2019-08-11 00:57:47 -0700

    Finished lesson7 hw

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.StackedInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+    fields = ('title', 'body', 'author', 'published_date')
+    list_display = ('title', 'author', 'created_date')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    exclude = ('posts',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
--- blogging/models.py
+++ blogging/models.py
@@ -12,10 +12,12 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
 class Category(models.Model):
   name = models.CharField(max_length=128)
   description = models.TextField(blank=True)
   posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+#   posts = models.ForeignKey(Post, on_delete=models.PROTECT)
 
   class Meta:
       verbose_name_plural = "Categories"
