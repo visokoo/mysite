3b7c3dfcb7af684df82fcd4fe3b223f8c0515995
visokoo <ta.vivian@gmail.com>
2019-08-15 23:15:36 -0700

    typo in production.py

--- mysite/production.py
+++ mysite/production.py
@@ -9,5 +9,5 @@ DATABASES = {
 DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS"), "localhost"]
-STATIC_ROOT = os.path.join(BASEDIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 SECRET_KEY = os.environ.get("SECRET_KEY")
